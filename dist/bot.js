d60d3e5054c8dcb359d37090e2bb94d4cd4316ba
Osminalx <93145043+Osminalx@users.noreply.github.com>
2023-05-23 11:10:23 -0700

    FIrst commit

---
+++ dist/bot.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const client = new discord_js_1.Client();
+discord_js_1.Client.login('MTA4NjM1MTM4NTA4OTU0NDMzMw.G2LFWj.q68Hiz32DSE4oIsl1RBkeWSJ_UcLanEm1FcbFg');
